0f654521e45e185216130853689921cc04512bba
SadiqCafarli61 <sadiqcafarlidret@gmail.com>
2023-05-07 11:30:25 +0400

    Travel website has been created

---
+++ web.js
@@ -0,0 +1,5 @@
+const buttonElementy = document.getElementById("dark")
+buttonElementy.addEventListener("click", function(){
+    document.body.style.backgroundColor = "#000"
+    document.body.style.color = "#fff"
+})
